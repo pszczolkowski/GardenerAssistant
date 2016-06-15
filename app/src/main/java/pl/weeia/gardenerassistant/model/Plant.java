3bff554662bbb7d99c5842d63bc623611017f101
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-15 12:44:27 +0200

    Add id fields to plants

--- app/src/main/java/pl/weeia/gardenerassistant/model/Plant.java
+++ app/src/main/java/pl/weeia/gardenerassistant/model/Plant.java
@@ -5,10 +5,19 @@ import java.util.List;
 
 public class Plant {
 
+	private int id;
 	private String name;
 	private String icon;
 	private boolean perennial;
 	private List<Action> actions;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
