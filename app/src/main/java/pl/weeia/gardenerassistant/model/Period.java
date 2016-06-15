7fbeb0117c1ae95cc9b642760dfd7ab841f514fa
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-15 09:56:45 +0200

    Create models corresponding to plants JSON data

---
+++ app/src/main/java/pl/weeia/gardenerassistant/model/Period.java
@@ -0,0 +1,23 @@
+package pl.weeia.gardenerassistant.model;
+
+public class Period {
+
+	private String from;
+	private String to;
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+}
