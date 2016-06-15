05da02dfa60e883303c1a9adb060c3654e4637f4
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-15 07:31:48 +0200

    Initial commit

---
+++ app/src/main/java/pl/weeia/gardenerassistant/ActionsActivity.java
@@ -0,0 +1,13 @@
+package pl.weeia.gardenerassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActionsActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_actions);
+	}
+}
