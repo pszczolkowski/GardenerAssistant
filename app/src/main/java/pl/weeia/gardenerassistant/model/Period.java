376f483b1ef5d4f0c680c42e2dcc5d46c9c6f21a
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-29 12:00:50 +0200

    Use yearless date in periods

--- app/src/main/java/pl/weeia/gardenerassistant/model/Period.java
+++ app/src/main/java/pl/weeia/gardenerassistant/model/Period.java
@@ -1,23 +1,25 @@
 package pl.weeia.gardenerassistant.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Calendar;
+
+import pl.weeia.gardenerassistant.util.YearlessDate;
+
 public class Period {
 
-	private String from;
-	private String to;
+	private YearlessDate from;
+	private YearlessDate to;
 
-	public String getFrom() {
-		return from;
+	@JsonCreator
+	public Period(@JsonProperty("from") String from, @JsonProperty("to") String to) {
+		this.from = stringToDate(from);
+		this.to = stringToDate(to);
 	}
 
-	public void setFrom(String from) {
-		this.from = from;
-	}
-
-	public String getTo() {
-		return to;
-	}
-
-	public void setTo(String to) {
-		this.to = to;
+	private YearlessDate stringToDate(String string) {
+		String[] stringParts = string.split("\\.");
+		return new YearlessDate(Integer.parseInt(stringParts[0], 10), Integer.parseInt(stringParts[1], 10));
 	}
 }
